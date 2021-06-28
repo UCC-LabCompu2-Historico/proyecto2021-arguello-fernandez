eac557944dee8588b40fd68c82e6e49b2c09543e
pedrofernandezmz <pedrofernandezmz@gmail.com>
2021-06-28 19:01:44 -0300

    Se agrego linea de la trayectoria

--- funciones.js
+++ funciones.js
@@ -1,5 +1,5 @@
 /**
- * Verifica los valores ingresados y calcula la altura maxima, alcance maximo y tiempo de vuelo.
+ * Verifica los valores ingresados y calcula la altura maxima, alcance maximo y tiempo de vuelo. Ademas cambia unidades.
  * @method calculo
  * @return altura,alcance,tiempo
  */
@@ -29,21 +29,21 @@ function calculo() {
     var angulo = document.getElementById('input_angulo').value;
 
     if (document.getElementById('unidadvelocidad').value == "km/h") {
-        velocidad=velocidad/3.6;
+        velocidad = velocidad / 3.6;
     }
 
     if (document.getElementById('unidadangulo').value == "Grad") {
-        angulo=angulo*Math.PI/180;
+        angulo = angulo * Math.PI / 180;
     }
 
-    var altura = (((Math.pow(velocidad,2))*(Math.pow(Math.sin(angulo),2)))/(2*9.8)).toFixed(2);
+    var altura = (((Math.pow(velocidad, 2)) * (Math.pow(Math.sin(angulo), 2))) / (2 * 9.8)).toFixed(2);
     document.getElementById("altura").value = altura;
 
 
-    var alcance = (((Math.pow(velocidad,2))*(Math.sin((angulo)*2)))/(9.8)).toFixed(2);
+    var alcance = (((Math.pow(velocidad, 2)) * (Math.sin((angulo) * 2))) / (9.8)).toFixed(2);
     document.getElementById("alcance").value = alcance;
 
-    var tiempo = (((2*velocidad)*(Math.sin(angulo)))/(9.8)).toFixed(2);
+    var tiempo = (((2 * velocidad) * (Math.sin(angulo))) / (9.8)).toFixed(2);
     document.getElementById("tiempo").value = tiempo;
 
     simulacion(velocidad, angulo, tiempo);
@@ -59,30 +59,52 @@ function calculo() {
  * @return x,y
  */
 
-var t=0;
+var t = 0;
 
 
 function simulacion(velocidad, angulo, tiempo) {
 
 
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    var x = velocidad*Math.cos(angulo);
-    var y = velocidad*Math.sin(angulo);
+    var canvas1 = document.getElementById("canvas1");
+    var ctx = canvas1.getContext("2d");
+    var canvas2 = document.getElementById("canvas2");
+    var pelota = canvas2.getContext("2d");
+    var x = velocidad * Math.cos(angulo);
+    var y = velocidad * Math.sin(angulo);
 
-    canvas.width = canvas.width;
+    ctx.beginPath();
+    ctx.fillStyle = "#f5f5f5";
+    ctx.arc((145 + (x * t)), 425 - (y * t - 0.5 * 9.8 * (Math.pow(t, 2))), 5, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.fill();
+
+    canvas2.width = canvas2.width;
 
     var img = new Image();
     img.src = "Imagenes/pelota.png";
-    ctx.beginPath();
-    ctx.drawImage(img, (125 + (x*t)), 400 - (y*t - 0.5 * 9.8 * (Math.pow(t,2))))
-    ctx.closePath();
+    pelota.beginPath();
+    pelota.drawImage(img, (125 + (x * t)), 400 - (y * t - 0.5 * 9.8 * (Math.pow(t, 2))))
+    pelota.closePath();
 
     t = t + 0.05;
 
     if (t < tiempo) {
-        setTimeout(function () {simulacion(velocidad, angulo, tiempo);}, 50);
+        setTimeout(function () {
+            simulacion(velocidad, angulo, tiempo);}, 50);
     } else {
-        t=0;
+        t = 0;
     }
+}
+
+/**
+ * Borra el canvas.
+ * @method borrar
+ * @return canvas1,canvas2
+ */
+
+function borrar() {
+    var canvas1 = document.getElementById("canvas1");
+    var canvas2 = document.getElementById("canvas2");
+    canvas1.width = canvas1.width;
+    canvas2.width = canvas2.width;
 }
